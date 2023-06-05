fa958536bcf6310e331947c109dd8c0db884088f
w.siteee <w.siteee@gmail.com>
2023-06-05 19:35:57 +0300

    update

--- build/esm/index.d.ts
+++ build/esm/index.d.ts
@@ -2,6 +2,9 @@
  * Type alias for possible biometry types
  */
 export declare type BiometryType = 'TouchID' | 'FaceID' | 'Biometrics';
+interface RNBiometricsOptions {
+    allowDeviceCredentials?: boolean;
+}
 interface IsSensorAvailableResult {
     available: boolean;
     biometryType?: BiometryType;
@@ -11,9 +14,6 @@ interface CreateKeysResult {
     publicKey: string;
 }
 interface BiometricKeysExistResult {
-    keysExist: boolean;
-}
-interface BiometricKeysExistAndSignatureValidResult {
     keysExist: boolean;
 }
 interface DeleteKeysResult {
@@ -31,25 +31,34 @@ interface CreateSignatureResult {
 }
 interface SimplePromptOptions {
     promptMessage: string;
+    fallbackPromptMessage?: string;
     cancelButtonText?: string;
 }
 interface SimplePromptResult {
     success: boolean;
     error?: string;
 }
-declare module ReactNativeBiometrics {
-    /**
-     * Enum for touch id sensor type
-     */
-    const TouchID = "TouchID";
-    /**
-     * Enum for face id sensor type
-     */
-    const FaceID = "FaceID";
-    /**
-     * Enum for generic biometrics (this is the only value available on android)
-     */
-    const Biometrics = "Biometrics";
+interface BiometricKeysExistAndSignatureValidResult {
+    keysExist: boolean;
+}
+/**
+ * Enum for touch id sensor type
+ */
+export declare const TouchID = "TouchID";
+/**
+ * Enum for face id sensor type
+ */
+export declare const FaceID = "FaceID";
+/**
+ * Enum for generic biometrics (this is the only value available on android)
+ */
+export declare const Biometrics = "Biometrics";
+export declare const BiometryTypes: {
+    TouchID: string;
+    FaceID: string;
+    Biometrics: string;
+};
+export declare module ReactNativeBiometricsLegacy {
     /**
      * Returns promise that resolves to an object with object.biometryType = Biometrics | TouchID | FaceID
      * @returns {Promise<Object>} Promise that resolves to an object with details about biometrics available
@@ -64,15 +73,9 @@ declare module ReactNativeBiometrics {
     /**
      * Returns promise that resolves to an object with object.keysExists = true | false
      * indicating if the keys were found to exist or not
-     * @returns {Promise<Object>} Promise that resolves to object with details aobut the existence of keys
+     * @returns {Promise<Object>} Promise that resolves to object with details about the existence of keys
      */
     function biometricKeysExist(): Promise<BiometricKeysExistResult>;
-    /**
-     * Returns promise that resolves to an object with object.keysExists = true | false
-     * indicating if the keys were found to exist or not and signature valid
-     * @returns {Promise<Object>} Promise that resolves to object with details aobut the existence of keys
-     */
-    function biometricKeysExistAndSignatureValid(): Promise<BiometricKeysExistAndSignatureValidResult>;
     /**
      * Returns promise that resolves to an object with true | false
      * indicating if the keys were properly deleted
@@ -86,7 +89,6 @@ declare module ReactNativeBiometrics {
      * @param {Object} createSignatureOptions
      * @param {string} createSignatureOptions.promptMessage
      * @param {string} createSignatureOptions.payload
-     * @param {string} createSignatureOptions.cancelButtonText (Android only)
      * @returns {Promise<Object>}  Promise that resolves to an object cryptographic signature details
      */
     function createSignature(createSignatureOptions: CreateSignatureOptions): Promise<CreateSignatureResult>;
@@ -96,9 +98,66 @@ declare module ReactNativeBiometrics {
      * object.success = false if the user cancels, and rejects if anything fails
      * @param {Object} simplePromptOptions
      * @param {string} simplePromptOptions.promptMessage
-     * @param {string} simplePromptOptions.cancelButtonText (Android only)
+     * @param {string} simplePromptOptions.fallbackPromptMessage
      * @returns {Promise<Object>}  Promise that resolves an object with details about the biometrics result
      */
     function simplePrompt(simplePromptOptions: SimplePromptOptions): Promise<SimplePromptResult>;
 }
-export default ReactNativeBiometrics;
+export default class ReactNativeBiometrics {
+    allowDeviceCredentials: boolean;
+    /**
+     * @param {Object} rnBiometricsOptions
+     * @param {boolean} rnBiometricsOptions.allowDeviceCredentials
+     */
+    constructor(rnBiometricsOptions?: RNBiometricsOptions);
+    /**
+     * Returns promise that resolves to an object with object.biometryType = Biometrics | TouchID | FaceID
+     * @returns {Promise<Object>} Promise that resolves to an object with details about biometrics available
+     */
+    isSensorAvailable(): Promise<IsSensorAvailableResult>;
+    /**
+     * Creates a public private key pair,returns promise that resolves to
+     * an object with object.publicKey, which is the public key of the newly generated key pair
+     * @returns {Promise<Object>}  Promise that resolves to object with details about the newly generated public key
+     */
+    createKeys(): Promise<CreateKeysResult>;
+    /**
+     * Returns promise that resolves to an object with object.keysExists = true | false
+     * indicating if the keys were found to exist or not
+     * @returns {Promise<Object>} Promise that resolves to object with details aobut the existence of keys
+     */
+    biometricKeysExist(): Promise<BiometricKeysExistResult>;
+    /**
+     * Returns promise that resolves to an object with object.keysExists = true | false
+     * indicating if the keys were found to exist or not and signature valid
+     * @returns {Promise<Object>} Promise that resolves to object with details aobut the existence of keys
+     */
+    biometricKeysExistAndSignatureValid(): Promise<BiometricKeysExistAndSignatureValidResult>;
+    /**
+     * Returns promise that resolves to an object with true | false
+     * indicating if the keys were properly deleted
+     * @returns {Promise<Object>} Promise that resolves to an object with details about the deletion
+     */
+    deleteKeys(): Promise<DeleteKeysResult>;
+    /**
+     * Prompts user with biometrics dialog using the passed in prompt message and
+     * returns promise that resolves to an object with object.signature,
+     * which is cryptographic signature of the payload
+     * @param {Object} createSignatureOptions
+     * @param {string} createSignatureOptions.promptMessage
+     * @param {string} createSignatureOptions.payload
+     * @returns {Promise<Object>}  Promise that resolves to an object cryptographic signature details
+     */
+    createSignature(createSignatureOptions: CreateSignatureOptions): Promise<CreateSignatureResult>;
+    /**
+     * Prompts user with biometrics dialog using the passed in prompt message and
+     * returns promise that resolves to an object with object.success = true if the user passes,
+     * object.success = false if the user cancels, and rejects if anything fails
+     * @param {Object} simplePromptOptions
+     * @param {string} simplePromptOptions.promptMessage
+     * @param {string} simplePromptOptions.fallbackPromptMessage
+     * @returns {Promise<Object>}  Promise that resolves an object with details about the biometrics result
+     */
+    simplePrompt(simplePromptOptions: SimplePromptOptions): Promise<SimplePromptResult>;
+}
+export {};
